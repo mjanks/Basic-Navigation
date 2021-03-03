b17d17b981385b42d6acc7693c8170c335450fe4
Michael Janks <mjanks@emich.edu>
2021-03-02 20:27:49 -0500

    added screens for navigation stacks

--- navigation/mainStackNav.js
+++ navigation/mainStackNav.js
@@ -1,122 +1,17 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import HomeScreen from "../screens/home";
-import SavedScreen from "../screens/saved";
-import ProfileScreen from "../screens/profile";
+import HomeStack from "./homeStack";
+import SavedStack from "./savedStack";
+import ProfileStack from "./profileStack";
 
-const HStack = createStackNavigator(); // creates the stack navigator 'HStack'
-const SaveStack = createStackNavigator();
-const ProfStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-
-function HomeStack() {
-  return (
-    <HStack.Navigator 
-        // set initial route, default is the first screen in the list below, 
-        // which is usually Home.
-        initialRouteName='Home'
-        // 'screenOptions' lets you apply common styles to the header across 
-        // the navigator
-        screenOptions={{
-          // allows swipe from left to right to go back one screen on Android, 
-          // IOS this is default behavior
-          gestureEnabled: true,
-          headerStyle: {
-            backgroundColor: '#101010'
-          },
-          headerTitleStyle: {
-            fontWeight: 'bold'
-          },
-          headerTintColor: '#ffd700',
-          // makes the back button title invisible on IOS,
-          // Android only shows the arrow so it's ok.
-          headerBackTitleVisible: false
-        }}
-        headerMode='none'
-      >
-        <HStack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ title: 'Home Screen' }}
-        />
-
-        {/* <Stack.Screen 
-          name="Details" 
-          component={DetailsScreen} 
-          // options={{ title: 'Details Screen' }}
-          options={({ route }) => ({
-            title: route.params.item.name
-          })}
-        /> */}
-
-      </HStack.Navigator>
-  );
-}
-
-function ProfileStack() {
-  return (
-    <ProfStack.Navigator 
-        initialRouteName='Profile'
-        screenOptions={{
-          gestureEnabled: true,
-          headerStyle: {
-            backgroundColor: '#101010'
-          },
-          headerTitleStyle: {
-            fontWeight: 'bold'
-          },
-          headerTintColor: '#ffd700',
-          headerBackTitleVisible: false
-        }}
-        headerMode='none'
-      >
-        <ProfStack.Screen 
-          name="Profile" 
-          component={ProfileScreen} 
-          options={{ title: 'Profile Screen' }}
-        />
-
-      </ProfStack.Navigator>
-  );
-}
-
-function SavedStack() {
-  return (
-    <SaveStack.Navigator 
-        initialRouteName='Saved'
-        screenOptions={{
-          gestureEnabled: true,
-          headerStyle: {
-            backgroundColor: '#101010'
-          },
-          headerTitleStyle: {
-            fontWeight: 'bold'
-          },
-          headerTintColor: '#ffd700',
-          headerBackTitleVisible: false
-        }}
-        headerMode='none'
-      >
-        <SaveStack.Screen 
-          name="Saved" 
-          component={SavedScreen} 
-          options={{ title: 'Saved Screen' }}
-        />
-
-      </SaveStack.Navigator>
-  );
-}
-
 
 function MainStackNav() {
   return (
     <NavigationContainer>
-      {/* Top level screen configuration. Add new screens here, then configure their 
-      details in separate js files in the screens folder */}
 
       <Tab.Navigator
         initialRouteName="HomeStack"
@@ -165,8 +60,6 @@ function MainStackNav() {
       </Tab.Navigator>
       
     </NavigationContainer>
-
-    
   );
 }
 
